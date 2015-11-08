51c796fc8961f61f6282a89103929956e2c8d185
marirod <mariolo94@hotmail.com>
2015-11-08 22:29:39 +0100

    Ahi va MyGLRenderer con un ejemplo de como iria funcionando la app, vamos como se mueve el mapa, de momento solo hay 3 laminas, un cesped, una carretera y un rio que se van moviendo , la idea seria que se generaran aleatoriamente sin fin

--- app/src/main/java/com/uva/moviles/savethefrog/MyGLRenderer.java
+++ app/src/main/java/com/uva/moviles/savethefrog/MyGLRenderer.java
@@ -3,6 +3,7 @@ package com.uva.moviles.savethefrog;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.util.Log;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -21,12 +22,17 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     //TODO: Deteccion del evento de deslizamiento y su direccion
     //TODO: Metodo de movimiento de la rana (salto = Parabola?)
     //TODO: Cambiar la camara de sitio (Vista de las laminas del suelo al estilo crossy road)
-    //TODO: Metodo generacion aleatoria de terreno
+    //TODO: Metodo generacion aleatoria de cesped
 
 
-    private Terreno terreno;
+
+    private Terreno cesped;
+    private Terreno carretera;
+    private Terreno rio;
     //Color Verde en formato R G B A 0 = min 1 = max
-    private float[] colorCesped = { 0.0f, 1.0f,0.0f,1.0f};
+    private float[] colorCesped = { 0.0f, 0.5f,0.0f,1.0f};
+    private float[] colorCarretera = { 0.5f, 0.5f,0.5f,1.0f};
+    private float[] colorRio = { 0.0f, 1.0f,1.0f,1.0f};
 
     private final float[] mMVPMatrix = new float[16];
     private final float[] mProjectionMatrix = new float[16];
@@ -35,16 +41,26 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     private final float[] mTempMatrix = new float[16];
     private int[] _viewport;
 
+    private float movY = 4f;
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 
 
-        terreno = new Terreno();
+        cesped = new Terreno();
+        carretera = new Terreno();
+        rio = new Terreno();
 
-        Matrix.translateM(terreno.mModelMatrix, 0, 0.0f, 0.0f, -3.0f);
+        Matrix.setLookAtM(mViewMatrix, 0, 0f, 0f, 3f, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
 
+        Matrix.translateM(cesped.mModelMatrix, 0, -5f, 0.0f, -3.0f);
+
+        Matrix.translateM(rio.mModelMatrix, 0, -5f, 1.0f, -3.0f);
+
+        Matrix.translateM(carretera.mModelMatrix, 0, -5f, 2.0f, -3.0f);
 
     }
 
@@ -55,19 +71,45 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         _viewport = new int[]{0,0,width,height};
 
         float ratio = (float) width / height;
-        Matrix.frustumM(mProjectionMatrix,0,-ratio,ratio,-1,1,3,7);
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
+
+
+
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        Matrix.setLookAtM(mViewMatrix, 0, 0, 0, 3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        movY -= 0.001f;
+        Matrix.setLookAtM(mViewMatrix, 0, 2.5f, -1.0f, 3f, 2.5f, 0.8f, -3.0f, 0f, 2.0f, 0.0f);
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
+
+        Matrix.scaleM(mScalationMatrix, 0, cesped.mModelMatrix, 0, 20.0f, 1.0f, 1.0f);
+        Matrix.multiplyMM(mTempMatrix, 0, mMVPMatrix, 0, mScalationMatrix, 0);
+        cesped.draw(colorCesped, mTempMatrix);
+
+        Matrix.scaleM(mScalationMatrix, 0, carretera.mModelMatrix, 0, 20.0f, 1.0f, 1.0f);
+        Matrix.multiplyMM(mTempMatrix, 0, mMVPMatrix, 0, mScalationMatrix, 0);
+        carretera.draw(colorCarretera, mTempMatrix);
+
+        Matrix.scaleM(mScalationMatrix, 0, rio.mModelMatrix, 0, 20.0f, 1.0f, 1.0f);
+        Matrix.multiplyMM(mTempMatrix, 0, mMVPMatrix, 0, mScalationMatrix, 0);
+        rio.draw(colorRio, mTempMatrix);
 
         Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
 
-        Matrix.scaleM(mScalationMatrix, 0, terreno.mModelMatrix, 0, 1.0f, 2.0f, 1.0f);
-        Matrix.multiplyMM(mTempMatrix, 0, mMVPMatrix, 0, mScalationMatrix, 0);
-        terreno.draw(colorCesped, mTempMatrix);
+        Matrix.setIdentityM(cesped.mModelMatrix, 0);
+        Matrix.translateM(cesped.mModelMatrix, 0, -5f, movY, -3.0f);
+        Matrix.rotateM(cesped.mModelMatrix, 0, -20.0f, 0.0f, 0.0f, 1.0f);
+
+        Matrix.setIdentityM(rio.mModelMatrix, 0);
+        Matrix.translateM(rio.mModelMatrix, 0, -5f, movY + 1, -3.0f);
+        Matrix.rotateM(rio.mModelMatrix, 0, -20.0f, 0.0f, 0.0f, 1.0f);
+
+        Matrix.setIdentityM(carretera.mModelMatrix, 0);
+        Matrix.translateM(carretera.mModelMatrix, 0, -5f, movY + 2, -3.0f);
+        Matrix.rotateM(carretera.mModelMatrix, 0, -20.0f, 0.0f, 0.0f, 1.0f);
 
     }
 
